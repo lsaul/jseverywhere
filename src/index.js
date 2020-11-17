1e5ca5f02b33430bba58623a9d884ef52f4f2557
Lloyd Saulpaugh <lsaul@uberdesign.org>
2020-11-17 11:36:18 -0500

    initial commit - borked

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
 // index.js
 // This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+//Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+app.get('/', (req, res) => res.send('Hello World!!!'));
+
+app.listen({ port }, () =>
+  console.log(
+    'GraphQL Server running at http//localhost: ' + port + server.graphqlPath
+  )
+);
+
+//Apply the Apollo graphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+
+// Construct a schema using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!'
+  }
+};
